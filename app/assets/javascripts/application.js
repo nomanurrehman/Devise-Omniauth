c9c62656e941ba03579ffd1fb2041afebf885e07
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-04-24 16:24:02 +0500

    Update application javascript
    - Adds Bootstrap javascript
    - Adds Material Design Bootstrap javascript
    - Adds Tether javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+
+//= require MDBootstrap/js/tether.min.js
+//= require MDBootstrap/js/bootstrap.min.js
+//= require MDBootstrap/js/mdb.min.js
+
 //= require_tree .
