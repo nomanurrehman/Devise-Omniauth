8333b8aab6dcf4ea417791e8afe0f91d6120afaa
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-04-24 16:30:47 +0500

    Update application config
    - Adds vendor folder to static assets lookup path

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module DeviseOmniauth
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # setup components folder for asset lookup
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
