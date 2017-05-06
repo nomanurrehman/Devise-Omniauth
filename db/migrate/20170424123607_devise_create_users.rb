91e4293fe17ae36622bfc42d29e3f0a8d2e3c097
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-05-06 17:47:10 +0500

    Update devise create users migration

--- db/migrate/20170424123607_devise_create_users.rb
+++ db/migrate/20170424123607_devise_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :users do |t|
+    create_table :users do | t |
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -29,7 +29,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       t.string   :unlock_token # Only if unlock strategy is :email or :both
       t.datetime :locked_at
-
 
       t.timestamps null: false
     end
