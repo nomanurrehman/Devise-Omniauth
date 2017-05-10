5c6e8590ca431634d8da81f705b59c1eca72f689
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-05-10 15:40:52 +0500

    Update application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def get_alert_class(key)
+    digest = HashWithIndifferentAccess.new(notice: 'success', error: 'danger')
+    digest[key].present? ? digest[key] : key
+  end
 end
