b851492658a9c5d96d4117bb61de25a64796abe3
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-05-12 17:31:53 +0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'site#index'
+  get 'dashboard', to: 'dashboard#index', as: :dashboard_index_path
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
 end
