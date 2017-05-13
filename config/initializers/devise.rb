e127d28f7b9c2cd97ebf4f428c98e8c553a976a5
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-05-13 11:47:10 +0500

    Update devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV['EMAIL_TITLE'] + ' <' + ENV['EMAIL_ADDRESS'] + '>'
+  #config.mailer_sender = ENV['EMAIL_TITLE'] + ' <' + ENV['EMAIL_ADDRESS'] + '>'
+  config.mailer_sender = 'Devise Omniauth <awsomecasts@gmail.com>'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
